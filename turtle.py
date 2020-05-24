ab75e70ed4446cffb294d5be6b2121e284400e88
Jamie Rowe <jpkrowe@hotmail.co.uk>
2020-05-24 05:15:23 +0100

    fix

--- turtle.py
+++ turtle.py
@@ -1,30 +1,29 @@
 import random
 print("Hello! Welcome to the test")
 
-NoOfQuestions = input ('Choose the number of questions you wish to answer')
-for i in NoOfQuestions:
-    number1 = random.randint(1,100)
-    number2 = random.randint(1,100)
-    symbol = random.randint(1,4)
+NoOfQuestions = int(input('Choose the number of questions you wish to answer'))
+NoCorrect = 0
+i = 0
+while i < NoOfQuestions:
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    symbol = random.randint(1, 3)
     if symbol == 1:
-         question = number1 + number2
-        answer = input('what is' + number1 + ' + ' + number2)
+        question = number1 + number2
+        answer = int(input('what is ' + str(number1) + ' + ' + str(number2)))
     elif symbol == 2:
         question = number1 - number2
-        answer = input('what is' + number1 + ' - ' + number2)
+        answer = int(input('what is ' + str(number1) + ' - ' + str(number2)))
     elif symbol == 3:
         question = number1 * number2
-        answer = input('what is' + number1 + ' * ' + number2)
-    elif symbol == 4:
-        question = number1 / number2
-        answer = input('what is' + number1 + ' / ' + number2)
+        answer = int(input('what is ' + str(number1) + ' * ' + str(number2)))
     if answer == question:
         print("You got the question correct!")
-        NoCorrect = NoCorrect + 1
+        NoCorrect += 1
     else:
         print("You got the question wrong")
-
-print("You got " + NoCorrect + "questions correct " + " out of " + NoOfQuestions)
+    i += 1
+print("You got " + str(NoCorrect) + " questions correct out of " + str(NoOfQuestions))
 
 
 
